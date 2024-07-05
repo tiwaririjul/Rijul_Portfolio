9d27dd44c427295bb3b0536d8706b90d514fda2d
tiwaririjul <tiwaririjul7@gmail.com>
2024-07-05 01:08:51 -0700

    Portfolio

---
+++ src/components/sections/Skills.jsx
@@ -0,0 +1,271 @@
+import {
+  BiLogoNetlify,
+  BiLogoPostgresql,
+  BiLogoTypescript,
+} from "react-icons/bi";
+import { CgVercel } from "react-icons/cg";
+import { DiGithubAlt, DiMongodb } from "react-icons/di";
+import { FaDocker } from "react-icons/fa";
+import { SiRedux } from "react-icons/si";
+import {
+  FaAws,
+  FaCss3Alt,
+  FaDigitalOcean,
+  FaGitAlt,
+  FaGitlab,
+  FaHtml5,
+  FaNodeJs,
+  FaPython,
+  FaReact,
+  FaVuejs,
+} from "react-icons/fa6";
+import { IoLogoJavascript } from "react-icons/io";
+import { IoLogoFirebase } from "react-icons/io5";
+import {
+  SiAntdesign,
+  SiExpress,
+  SiFlask,
+  SiJest,
+  SiNuxtdotjs,
+  SiPassport,
+  SiPostman,
+  SiPythonanywhere,
+  SiSequelize,
+  SiTailwindcss,
+  SiTensorflow,
+} from "react-icons/si";
+import { TbBrandNextjs } from "react-icons/tb";
+
+function Skills() {
+  return (
+    <section id="skills" className="mx-4 lg:mx-20">
+      {/* =========== SKILLS TITLE =========== */}
+      <h4 className="text-4xl font-bold text-center mt-20 dark:text-white">
+        Tools and Technologies I Use.
+      </h4>
+      {/* =========== LIST OF SKILLS =========== */}
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-5 gap-4 mt-8">
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-2 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaHtml5 className="text-2xl" /> HTML
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            Used for creating the structure and content of web pages.
+          </span>
+        </span>
+
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaCss3Alt className="text-2xl" /> CSS
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            Used for styling and layout of web pages.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <IoLogoJavascript className="text-2xl" /> JavaScript
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A scripting language for creating dynamic and interactive web
+            content.
+          </span>
+        </span>
+
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaPython className="text-2xl" /> Java
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            Java is a versatile, object-oriented programming language that runs
+            on any device with the Java Virtual Machine (JVM). Known for its
+            platform independence, security, and robustness, it's widely used in
+            web development, mobile apps, and enterprise software.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaPython className="text-2xl" /> Python
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            Java is a versatile, object-oriented programming language that runs
+            on any device with the Java Virtual Machine (JVM). Known for its
+            platform independence, security, and robustness, it's widely used in
+            web development, mobile apps, and enterprise software.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaReact className="text-2xl" /> React
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A JavaScript library for building user interfaces, particularly
+            single-page applications.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiRedux className="text-2xl" /> Redux
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            Redux is a JavaScript library for managing application state, often
+            used with React. It centralizes state in a single store, making
+            state changes predictable and easier to debug.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiExpress className="text-2xl" /> Express
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A web application framework for NodeJS, used to build web and mobile
+            applications.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaNodeJs className="text-2xl" /> NodeJS
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A runtime environment that allows for the execution of JavaScript
+            code on the server side.
+          </span>
+        </span>
+
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiTailwindcss className="text-2xl" /> Tailwind
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A utility-first CSS framework for rapidly building custom user
+            interfaces.
+          </span>
+        </span>
+
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiJest className="text-2xl" /> Jest
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A React framework for building server-rendered React applications
+            and static websites.
+          </span>
+        </span>
+
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <FaGitAlt className="text-2xl" /> Git
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A version control system for tracking changes in source code during
+            software development.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <DiGithubAlt className="text-2xl" /> Github
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A platform for hosting and collaborating on Git repositories.
+          </span>
+        </span>
+
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <BiLogoNetlify className="text-2xl" /> Netlify
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A platform for deploying and hosting static websites and serverless
+            functions.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <CgVercel className="text-2xl" /> Vercel
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A platform for deploying and hosting frontend frameworks and static
+            sites.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <IoLogoFirebase className="text-2xl" /> Firebase
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A platform by Google for building mobile and web applications,
+            providing services like databases, authentication, and hosting.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <SiPostman className="text-2xl" /> Postman
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A collaboration platform for API development, testing, and
+            monitoring.
+          </span>
+        </span>
+
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <DiMongodb className="text-2xl" /> MongoDB
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            A NoSQL database used for storing and managing large sets of
+            unstructured data.
+          </span>
+        </span>
+        <span className="hs-tooltip [--placement:bottom] hs-tooltip-toggle inline-flex items-center justify-between gap-x-1.5 py-4 px-3 hover:-translate-y-1.5 transition-translate duration-500 rounded-xl font-medium bg-red-100 text-red-800 dark:bg-red-800/30 dark:text-red-500">
+          <BiLogoPostgresql className="text-2xl" /> SQL
+          {/* =========== TOOLTIP TEXT =========== */}
+          <span
+            className="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 py-1 px-2 bg-white border dark:border-zinc-800 dark:bg-zinc-950 text-xs font-medium text-zinc-950 dark:text-white rounded shadow-sm"
+            role="tooltip"
+          >
+            An open-source relational database management system known for its
+            extensibility and SQL compliance.
+          </span>
+        </span>
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
---
+++ src/data/user_info.js
@@ -0,0 +1,168 @@
+const info = {
+  // ============ MAIN DETAILS ============
+  main: {
+    name: "Rijul Tiwari",
+    description:
+      "I am a software engineer with expertise in full-stack web development. I have experience in building scalable, secure and reliable web applications using various frameworks and technologies. I enjoy solving complex problems and learning new skills. I am passionate about creating high-quality code that follows best practices and industry standards.",
+    role: "Full-Stack Developer",
+    photo: "../photo.webp",
+    email: "tiwaririjul7@gmail.com",
+  },
+
+  // ============ SOCIAL LINKS ============
+  socials: {
+    github: "https://github.com/tiwaririjul",
+    linkedin: "https://www.linkedin.com/in/rijul-tiwari-010407216/",
+    instagram: "https://www.instagram.com/tiwaririjul/",
+    facebook: "https://www.facebook.com/rijul.tiwari.50/",
+  },
+
+  // ============ PROJECTS ============
+  projects: [
+    {
+      title: "Course Bundler",
+      description:
+        "Created s responsive learning platform with integrated razor pay for smooth payment processing and also implemented Redux for efficient State management optimizing performance admin panel for seamless course and lecture management",
+      technologies: "React, ChakraUI, MongoDB, Nodejs , Expressjs",
+      github: "https://github.com/tiwaririjul/courseBundlerServer.git",
+      github: "https://github.com/tiwaririjul/courseBundlerServer.git",
+      link: "https://github.com/tiwaririjul/courseBundlerServer.git",
+    },
+
+    {
+      title: "ChatApp",
+      description:
+        "Developed a chat application in which user can create a personal chat room and have a conversation also added feature of voice and video call",
+      technologies: "React , Nodejs , Expressjs , MongoDB , Tailwind",
+      github: "https://github.com/tiwaririjul/chat.git",
+      link: "chatternet.onrender.com",
+    },
+
+    {
+      title: "Flipkart CLone",
+      description: "Created a responsive Flipkart clone with ",
+      technologies: "React, Tailwind, expressjs , Nodejs",
+      github: "https://github.com/dsbalico/letter-digit-shape-classification",
+      link: "https://github.com/dsbalico/letter-digit-shape-classification",
+    },
+
+    {
+      title: "Full Fledged Ecommerce backend system",
+      description:
+        "developed a backend system for an ecommerce website or managing various Catalogs and Subcatalogs , Managing Products creating orders and more",
+      technologies: "Nodejs , Expressjs , MongoDB",
+      github: "https://github.com/pradip8928/Giftana",
+      link: "https://github.com/pradip8928/Giftana",
+    },
+
+    {
+      title: "Sudoku Solver",
+      description:
+        "Sudoku Solver is a web application that can generate or solve sudoku problems using simulated annealing, an optimization algorithm.",
+      technologies: "React, Tailwind, TypeScript",
+      github:
+        "https://github.com/dsbalico/sudoku-solver-using-simulated-annealing",
+      link: "https://sudoku-solver-sa.netlify.app/",
+    },
+    {
+      title: "recipe App",
+      description:
+        "Recipe generater app in which user can create recipes filing detail and put live with proper user  authentication",
+      technologies: "React, Tailwind, TypeScript",
+      github:
+        "https://github.com/dsbalico/sudoku-solver-using-simulated-annealing",
+      link: "https://sudoku-solver-sa.netlify.app/",
+    },
+  ],
+
+  // ============ EDUCATION ============
+  education: [
+    {
+      school: "Vidyavardhini's College Of Engineering and Technology",
+      degree: "Bachelor of Engineering in Information Technology",
+      duration: "2020 - 2024",
+      image: "Collegelogo_1.webp",
+    },
+    {
+      school: "Shankar Narayan Junior College Of Engineering and Technology",
+      degree: "Stream Science (PCM)",
+      duration: "2018 - 2020",
+      image: "SN_logo.webp",
+    },
+  ],
+
+  // ============ EXPERIENCE ============
+  experience: [
+    {
+      position: "Full-Stack Developer Intern",
+      company: "Giftana India: India's Leading Corporate Gifting Company",
+      duration: "12th Dec 2022 - 12th Feb 2023 (2 Months)",
+      image: "Gitana.webp",
+      descriptions: [
+        "Completed an Real time based project in full-stack web development.",
+        "Acquired practical experience with React , Nodejs , Expressjs , MongoDB and many tools",
+        "Developed RESTful APIs using Express.js for back-end functionality.",
+        "Actively contributed to the ongoing development of a web application.",
+        "Utilized Git and GitLab for effective version control and team collaboration.",
+        "Gained Knowledge on development best practices, code optimization, and deployment.",
+        "Participated in Agile workflows for daily updates and team alignment.",
+      ],
+    },
+    {
+      position: "Web Designer",
+      company: "Sahu Technolgies",
+      duration: "15th July 2022 - 5th Aug 2022 (1.5 Month)",
+      image: "Sahu.webp",
+      descriptions: [
+        "Developed responsive  design template for a company website",
+        "Strategic planning and execution Team leadership and management conversation skills",
+      ],
+    },
+  ],
+
+  // ============ CERTIFICATES ============
+  certificates: [
+    {
+      title: "Bootcamp on Reactjs",
+      description: "Provided by ShapeAi",
+      icon: "",
+      link: "https://drive.google.com/drive/folders/16885_HhsyhCgbISorcorMdCL31pOJMRN",
+    },
+    {
+      title: "Back-End Engineer Virtual Experience",
+      description: "Provided by LYFT & Authorized by Forage",
+      icon: "",
+      link: "https://drive.google.com/file/d/15C-ajQBMc7EMO3GO9rjALi5yKzozJYSh/view",
+    },
+    {
+      title: "Software Engineer Virtual Experience",
+      description: "Provided by Commenwealth Bank & Authorized by Forage",
+      icon: "",
+      link: "https://drive.google.com/file/d/120BTWVA-7uAGq3lvRmYRS4kByzXpNqzX/view",
+    },
+    {
+      title: "Hackathon",
+      description: "Provided by Vcet",
+      icon: "",
+      link: "https://drive.google.com/drive/folders/16885_HhsyhCgbISorcorMdCL31pOJMRN",
+    },
+    {
+      title: "Soft Skill Development",
+      description: "Provided by Vcet",
+      icon: "",
+      link: "https://drive.google.com/drive/folders/16885_HhsyhCgbISorcorMdCL31pOJMRN",
+    },
+  ],
+
+  // ============ CONTACT ============
+  contact: {
+    title: "Let's Get in Touch: Ways to Connect with Me",
+    description:
+      "Thank you for your interest in getting in touch with me. I welcome your feedback, questions, and suggestions. If you have a specific question or comment, please feel free to email me directly at tiwaririjul7@gmail.com. I make an effort to respond to all messages within 24 hours, although it may take me longer during busy periods. Finally, if you prefer to connect on social media, you can connect with me using the social media links below.",
+  },
+
+  // ============ FOOTER ============
+  footer: "© 2024 Rijul Tiwari. All Rights Reserved",
+};
+
+export default info;
